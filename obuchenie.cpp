cd4b83297fa56201d10c00cc7628d5dac9353bb8
Nikita-Syabruyk <90602552+Nikita-Syabruyk@users.noreply.github.com>
2022-07-16 12:48:20 +0300

    Add files via upload

---
+++ obuchenie.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int** arr = new int* [n];
+	for (int i = 0; i < n; i++) {
+		arr[i] = new int[n];
+	}
+	int kol = 1;
+	for (kol; kol < n; kol++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == j) {
+					arr[i][j] = 0;
+				}
+				if (arr[i][j-1]==(kol-1)) {
+					arr[i][j] = kol;
+				}
+			}
+		}
+	}
+	kol = 1;
+	for (kol; kol < n; kol++) {
+		for (int i = 0; i <n; i++) {
+			for (int j =n-1; j > -1; j--) {
+				if (arr[i][j + 1] == (kol - 1) && arr[i][j] != NULL&&arr[i][j]<0) {
+					arr[i][j] = kol;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	delete[]arr;
+}
